767e17ae313ce4c996e18f406164daaf640c7510
essemfly <essemfly@gmail.com>
2017-09-22 11:40:38 +0900

    Make async server calls for login and authenticate services

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../_services/index';
 
 @Component({
     selector: 'login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
+    providers: [ AuthenticationService],
 })
-export class LoginComponent { }
+export class LoginComponent implements OnInit {
+    model: any = {
+        username: '',
+        password: ''
+    };
+    loading = false;
+    error = '';
+ 
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService) { }
+ 
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.logout();
+    }
+ 
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(result => {
+                if (result === true) {
+                    // login successful
+                    this.router.navigate(['/']);
+                } else {
+                    // login failed
+                    this.error = 'Username or password is incorrect';
+                    this.loading = false;
+                }
+            });
+    }
+}
