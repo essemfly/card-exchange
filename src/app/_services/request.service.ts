9d58af39eff9e07e91e125f33008f7584e548cac
essemfly <essemfly@gmail.com>
2017-09-25 16:50:41 +0900

    Get requests from users

--- src/app/_services/request.service.ts
+++ src/app/_services/request.service.ts
@@ -5,7 +5,17 @@ import 'rxjs/add/operator/map'
 @Injectable()
 export class RequestService {
     constructor(
-        private http: Http) {}
+        private http: Http
+    ) {}
+
+    getRequests(token) {
+        let headers = new Headers({ 'Authorization': 'JWT ' + token });
+        let options = new RequestOptions({ headers: headers });
+        return this.http.get('http://localhost:8000/exchange/requests/', options)
+            .map((response: Response) => {
+                return response.json()
+            })
+    }
 
     postRequest(token, haveCard, wantCard) {
         let headers = new Headers({ 'Authorization': 'JWT ' + token });
@@ -15,8 +25,8 @@ export class RequestService {
             "want_card_id" : wantCard
         };
         return this.http.post('http://localhost:8000/exchange/requests/', postData, options)
-        .map((response: Response) => {
-            return response.json()
-        });
+            .map((response: Response) => {
+                return response.json()
+            });
     }
 }
