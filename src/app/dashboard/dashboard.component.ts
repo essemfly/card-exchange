05da23e9cd46b4d80e18412bff99c64676197db2
essemfly <essemfly@gmail.com>
2017-09-25 18:21:28 +0900

    Make dashboard page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,6 +13,7 @@ import { RequestService, AuthenticationService } from '../_services/index';
 })
 export class DashboardComponent implements OnInit {
     public requests = []
+    public requestResults = []
     constructor(
         private requestService: RequestService,
         private authenticationService: AuthenticationService,
@@ -26,6 +27,35 @@ export class DashboardComponent implements OnInit {
         this.requestService.getRequests(this.authenticationService.token)
             .subscribe(requests => {
                 this.requests = requests
+                this.convertRequestData()
             })
+    }
+
+    convertRequestData() {
+        for (let i=0; i<this.requests.length; i++) {
+            this.requestResults.push({
+                "id": this.requests[i].id,
+                "have_card": this.requests[i].have_card.member + " / " + this.requests[i].have_card.type + " / " + this.requests[i].have_card.version,
+                "want_card": this.requests[i].want_card.member + " / " + this.requests[i].want_card.type + " / " + this.requests[i].want_card.version,
+                "created_at": this.convertTime(this.requests[i].created_date),
+                "status": this.convertStatus(this.requests[i].status),
+            })
+        }
+    }
+
+    convertStatus(status) {
+        switch(status) {
+            case 0:
+                return '취소됨'
+            case 1:
+                return '대기중'
+            case 2:
+                return '매칭됨'
+        }
+    }
+
+    convertTime(time) {
+        let newTime = new Date(time)
+        return newTime.getFullYear() + '년 ' + newTime.getMonth() + '월 ' + newTime.getDate() + '일 ' + newTime.getHours() + '시 ' + newTime.getMinutes() + '분'
     }
 }
